4da3d82cce9868c7fb41ff1b503eac231ef2c0ca
Usama Arshad <usamaal320@gmail.com>
2026-08-23 18:36:04 +0500

    refactor: separate workflow node from graph definition

--- src/travel_planner/graph.py
+++ src/travel_planner/graph.py
@@ -1,14 +1,15 @@
 from langgraph.graph import END, START, StateGraph
 
+from travel_planner.nodes import initialize_request
 from travel_planner.state import TravelPlannerState
 
 
 def build_graph():
     graph = StateGraph(TravelPlannerState)
 
-    graph.add_node("start", lambda state: state)
+    graph.add_node("initialize_request", initialize_request)
 
-    graph.add_edge(START, "start")
-    graph.add_edge("start", END)
+    graph.add_edge(START, "initialize_request")
+    graph.add_edge("initialize_request", END)
 
     return graph.compile()
---
+++ src/travel_planner/nodes.py
@@ -0,0 +1,5 @@
+from travel_planner.state import TravelPlannerState
+
+
+def initialize_request(state: TravelPlannerState) -> TravelPlannerState:
+    return state
